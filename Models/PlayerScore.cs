015813be598068e0f022fcaed88dd4b7b894d460
Administrator <Administrator@XDSION>
2025-03-10 02:04:15 +0800

    111

--- Models/PlayerScore.cs
+++ Models/PlayerScore.cs
@@ -8,14 +8,6 @@
         public ScoreLev Level { get; set; }
         public string? TrackName { get; set; }
     }
-    public enum ScoreLev 
-    {
-        None,
-        主力,
-        一线,
-        二线,
-        三线,
-        娱乐,
-    }
+
 
 }
